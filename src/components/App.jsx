0517d240c70903f80771971510cb9ca5d8496eca
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-16 15:31:44 +0200

    fixed fetching on reload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
   const [largeImgUrl, setLargeImgUrl] = useState('');
 
   useEffect(() => {
+    if (!search) return;
     const fetchData = async () => {
       try {
         setIsLoading(true);
